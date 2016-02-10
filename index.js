4febfdaaabb83cd6dc9353429b35b768d46df165
Daniel Fanara <fanardat16@gmail.com>
2016-02-10 16:10:05 -0500

    Finishing Touches

--- index.js
+++ index.js
@@ -12,17 +12,22 @@ app.post('/report', function(req, res) {
 	console.log(req.body.log.length);
 	var name = rando.generate(6);
 	console.log(name);
+	var dat = req.body;
+	console.log(req.headers);
 	fs.writeFile("reports/" + name + ".json", JSON.stringify(req.body, null, 4));
 	res.send({url: "http://rdb.shdwlf.com/" + name});
 });
 
 app.get('/:id', function(req, res){
-	res.render('report');
+	res.render('report', {data: require('./reports/' + req.params.id + ".json")});
 });
 
 app.get('/', function(req, res) {
-	console.log('index');
-	res.send("wooho");
+	res.render('index');
+});
+
+app.get('/contact', function(req, res) {
+	res.render('contact');
 });
 
 app.listen(8001, function() {
