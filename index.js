26d3b137e00e951675723e1aee9a218d4bac9fe9
Daniel Fanara <fanardat16@gmail.com>
2016-02-10 16:48:04 -0500

    Updated module, added ip logging, better debug.

--- index.js
+++ index.js
@@ -4,22 +4,43 @@ var app = express();
 var bodyParser = require('body-parser');
 var rando = require('randomstring');
 
+function friendlyTimestamp() {
+  var time = (new Date() + "").split(" ");
+  time.splice(5);
+  var t = "";
+  for(var i = 0; i < time.length; i++) {
+    t = t + time[i] + " ";
+  }
+  return t.trim();
+}
+
 app.use(bodyParser.json({limit: '2mb'}));
+app.use(function(req, res, next) {
+	if(req.headers['cf-connecting-ip']) {
+    req._ip = req.headers['cf-connecting-ip'];
+  }else {
+    req._ip = req.ip;
+  }
+
+	console.log("[" + friendlyTimestamp() + "] " + req.path + " " + req.method + " " + req._ip);
+	next();
+});
 app.set('view engine', 'jade');
 
 app.post('/report', function(req, res) {
-	console.log('new request!');
-	console.log(req.body.log.length);
 	var name = rando.generate(6);
-	console.log(name);
 	var dat = req.body;
-	console.log(req.headers);
+	dat.ip = req._ip;
 	fs.writeFile("reports/" + name + ".json", JSON.stringify(req.body, null, 4));
 	res.send({url: "http://rdb.shdwlf.com/" + name});
 });
 
 app.get('/:id', function(req, res){
-	res.render('report', {data: require('./reports/' + req.params.id + ".json")});
+	if(fs.existsSync('./reports/' + req.params.id + ".json")) {
+		res.render('report', {data: require('./reports/' + req.params.id + ".json")});
+	}else {
+		res.status(404).send("That report was not found.");
+	}
 });
 
 app.get('/', function(req, res) {
